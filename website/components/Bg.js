5f58aafcc089538c4d718315809a8910815d6ec8
Boboch D. Brew <49965312+BobochD-Brew@users.noreply.github.com>
2022-06-15 23:02:11 +0100

    ss

--- website/components/Bg.js
+++ website/components/Bg.js
@@ -1,9 +1,21 @@
+import {useState, useEffect} from "react";
+
 const Bg = () => {
+  const [count, setCount] = useState(0);
+
+  let innerHeight = globalThis.innerHeight;
+  let innerWidth = globalThis.innerWidth;
+  useEffect(() => {
+    window.addEventListener('resize', () => {   
+      setCount(innerHeight+innerWidth);
+    })
+  },[])
   return (
     <div
-      className="-z-10 fixed inset-0 font-bold flex justify-center items-center mb-[250px]"
+      className="-z-10 fixed inset-0 font-bold flex text-[#74d9ff] justify-center items-center mb-[250px]"
       style={{
-        fontSize: "350px",
+        fontSize: innerHeight > innerWidth ? "10vw" : "40vh",
+        textShadow: "#FFF 0px 0px 15px, #74d9ff 0px 0px 5px, #74d9ff 0px 0px 10px, #74d9ff 0px 0px 20px, #74d9ff 0px 0px 30px, #74d9ff 0px 0px 50px"
       }}
     >
       HODLHQ
--- website/components/Navbar.js
+++ website/components/Navbar.js
@@ -1,10 +1,13 @@
+import Image from "next/image";
+import Logo from "public/logo.png";
+
 const HeaderNav = () => {
   return (
     <div className="flex justify-between items-center">
-      <div className="w-20">
-        <img src="https://www.coolxclones.xyz/favicon.ico" alt="HODLHQ" />
+      <div className="mt-4 w-20">
+        <Image src={Logo} layout="" objectFit="contain" />
       </div>
-      <div className="flex items-center space-x-10">
+      <div className="flex text-[#74d9ff] font-bold uppercase items-center space-x-10">
         <div>Our values</div>
         <div>The Brand</div>
         <div>The Team</div>
